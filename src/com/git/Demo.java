c89db4fab23a5630e41c893511af270f0fa2eabe
Lenovo <Lenovo@LAPTOP-3F095K3Q>
2019-05-20 15:17:45 +0800

    第四次

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -3,7 +3,7 @@ package com.git;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("cnmm");
+		System.out.println("cnmmm");
 		System.out.println("git6ÊÇ");
 	}
 
